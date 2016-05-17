63c76644dce093e32b67fbed44d96b208238d8bf
aerge <48838965@qq.com>
2016-05-17 17:38:32 +0800

    重新梳理目录结构

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    ypcjs = require("../models/ypcjData.json");
+    router = express.Router(),
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* 72PX场景应用*/
+router.get('/ypcj', function(req, res, next) {
+    //查询约拍场景
+    res.render('ypcj', {
+        title: '72PX约拍场景',
+        ypcjs : ypcjs
+    });
 });
 
 module.exports = router;
